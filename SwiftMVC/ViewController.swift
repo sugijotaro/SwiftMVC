9c72aeea27589e5100fd92476e19a86d701c54c4
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-02-02 14:10:00 +0900

    Initial Commit

---
+++ SwiftMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftMVC
+//
+//  Created by JotaroSugiyama on 2023/02/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
